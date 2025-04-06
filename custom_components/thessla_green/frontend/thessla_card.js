8a60f9a2d8111cd4399705be3f58b4277b65ed38
SzakalLDZ <alan.j.com@gmail.com>
2025-04-06 13:47:52 +0200

    small fix

--- custom_components/thessla_green/frontend/thessla_card.js
+++ custom_components/thessla_green/frontend/thessla_card.js
@@ -1,38 +1,7 @@
 class ThesslaPictureCard extends HTMLElement {
-    setConfig(config) {
-      this._config = config;
-  
-      const content = document.createElement("hui-picture-elements-card");
-      content.setConfig({
-        image: "/custom_components/thessla_green/frontend/reku.png",
-        elements: [
-          {
-            type: "state-badge",
-            entity: "binary_sensor.remote_ui",
-            style: {
-              top: "32%",
-              left: "40%"
-            }
-          }
-        ]
-      });
-  
-      const card = document.createElement("ha-card");
-      card.appendChild(content);
-      this.innerHTML = "";
-      this.appendChild(card);
-    }
-  
-    set hass(hass) {
-      this._hass = hass;
-      if (this.firstChild && this.firstChild.firstChild) {
-        this.firstChild.firstChild.hass = hass;
-      }
-    }
-  
-    getCardSize() {
-      return 3;
-    }
+  setConfig(config) {
+    this.innerHTML = `<ha-card><div style="padding: 1em;">Karta działa!</div></ha-card>`;
   }
-  
-  customElements.define("thessla-picture-card", ThesslaPictureCard);  
+
+  set hass(hass) {}
+}
