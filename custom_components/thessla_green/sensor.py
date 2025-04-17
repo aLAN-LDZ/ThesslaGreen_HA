368cefa3ad7ef45be7eaa66248c06955769df218
aLAN <33158372+aLAN-LDZ@users.noreply.github.com>
2025-04-17 18:14:56 +0200

    2456

--- custom_components/thessla_green/sensor.py
+++ custom_components/thessla_green/sensor.py
@@ -1,41 +1,21 @@
 from homeassistant.components.sensor import SensorEntity
-from homeassistant.const import UnitOfTemperature, UnitOfVolume, PERCENTAGE
+from homeassistant.const import UnitOfTemperature
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.core import HomeAssistant
 from homeassistant.config_entries import ConfigEntry
 from .const import DOMAIN
 
+# Definicja tylko jednego sensora
 SENSOR_DEFS = [
     ("Temperatura Czerpnia", 16, "input", UnitOfTemperature.CELSIUS, 0.1, 1),
-    ("Temperatura Nawiew", 17, "input", UnitOfTemperature.CELSIUS, 0.1, 1),
-    ("Temperatura Wywiew", 18, "input", UnitOfTemperature.CELSIUS, 0.1, 1),
-    ("Temperatura za FPX", 19, "input", UnitOfTemperature.CELSIUS, 0.1, 1),
-    ("Temperatura PCB", 22, "input", UnitOfTemperature.CELSIUS, 0.1, 1),
-    ("Strumień nawiew", 256, "holding", "m³/h", 1, 1),
-    ("Strumień wywiew", 257, "holding", "m³/h", 1, 1),
-    ("speedmanual", 4210, "holding", PERCENTAGE, 1, 1),
-    # dodajemy pozostałe jako surowe liczby
-    ("tryb pracy", 4208, "holding", None, 1, 0),
-    ("lato zima", 4209, "holding", None, 1, 0),
-    ("Bypass", 4320, "holding", None, 1, 0),
-    ("fpx flaga", 4192, "holding", None, 1, 0),
-    ("FPX tryb", 4198, "holding", None, 1, 0),
-    ("Alarm", 8192, "holding", None, 1, 0),
-    ("Error", 8193, "holding", None, 1, 0),
-    ("FPX zabezpieczenie termiczne", 8208, "holding", None, 1, 0),
-    ("Awaria Wentylatora Nawiewu", 8222, "holding", None, 1, 0),
-    ("Awaria Wentylatora Wywiewu", 8223, "holding", None, 1, 0),
-    ("Awaria CF Nawiewu", 8330, "holding", None, 1, 0),
-    ("Awaria CF Wywiewu", 8331, "holding", None, 1, 0),
-    ("Wymiana Filtrów", 8444, "holding", None, 1, 0),
 ]
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback):
     """Konfiguracja sensorów dla wpisu w UI."""
     handler = hass.data[DOMAIN]["handler"]
     
-    # Tworzenie sensorów na podstawie definicji
+    # Tworzenie sensorów na podstawie definicji (tylko jeden sensor)
     sensors = [
         ThesslaGreenSensor(name, address, input_type, unit, scale, precision, handler)
         for name, address, input_type, unit, scale, precision in SENSOR_DEFS
